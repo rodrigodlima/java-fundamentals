a9aaee3834fbc55c80313be8d1ab186ffad5a18a
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-09-27 16:41:36 -0300

    Examples for mathematics

---
+++ Execises/exercices/src/Mathematics.java
@@ -0,0 +1,22 @@
+public class Mathematics {
+    public static void main(String[] args) {
+        float numberOne = 20;
+        float numberTwo = 10;
+
+        // Addition
+        float totalSoma = numberOne + numberTwo;
+        System.out.println(totalSoma);
+
+        // Subtraction
+        float totalSubstracao = numberOne - numberTwo;
+        System.out.println(totalSubstracao);
+
+        // Multiplication
+        float totalMulti = numberOne * numberTwo;
+        System.out.println(totalMulti);
+
+        // Division
+        float totalDiv = numberOne / numberTwo;
+        System.out.println(totalDiv);
+    }
+}
