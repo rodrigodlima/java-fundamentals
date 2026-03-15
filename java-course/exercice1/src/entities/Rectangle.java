a12bce3dde80fad2b134796957d4cfbca6943f95
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-03-14 22:29:45 -0300

    Add java project execercice 1

---
+++ java-course/exercice1/src/entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Rectangle {
+	public double width;
+	public double height;
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return 2 * (width + height);
+	}
+	
+	public double diagonal() {
+		double c = (width * width) + (height * height);
+		double total = Math.sqrt(c);
+		return total;
+	}
+
+}
