0a5f60cb2dd40b35dda1e5fc2711a0c5b103d17e
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-03-15 14:53:14 -0300

    Add exercise 3 implementation with Program and Student classes

--- java-course/exercise3/src/application/Program.java
+++ java-course/exercise3/src/application/Program.java
@@ -1,4 +1,25 @@
 package application;
 
+import entities.Student;
+
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Student stud = new Student();
+        System.out.println("Enter the name of student");
+        stud.name = sc.nextLine();
+        System.out.println("Enter the grades");
+        stud.grade1 = sc.nextDouble();
+        stud.grade2 = sc.nextDouble();
+        stud.grade3 = sc.nextDouble();
+        System.out.printf("Final grade = %.2f%n", stud.sumGrade());
+        if (stud.sumGrade() >= 60){
+            System.out.println("PASS");
+        } else {
+            double calc = 60.00 - stud.sumGrade();
+            System.out.printf("MISSING %.2f POINTS", calc);
+        }
+    }
 }
--- java-course/exercise3/src/entities/Student.java
+++ java-course/exercise3/src/entities/Student.java
@@ -1,4 +1,12 @@
 package entities;
 
 public class Student {
+    public String name;
+    public double grade1;
+    public double grade2;
+    public double grade3;
+
+    public double sumGrade(){
+        return grade1 + grade2 + grade3;
+    }
 }
